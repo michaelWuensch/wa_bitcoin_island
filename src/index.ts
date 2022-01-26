6b48d13b2b7adb17e1333919e4872941a4476a7e
michaelWuensch <michael90@protonmail.com>
2022-01-26 20:45:54 +0000

    Deploying to gh-pages from master @ 5ff0497dfe69edc009d44b640647a164c2768c23 🚀

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,24 @@ bootstrapExtra().catch(e => console.error(e));
 let currentPopup: any = undefined;
 
 WA.room.onEnterLayer('scheduleZone').subscribe(() => {
-    console.log('toto')
-    currentPopup =  WA.ui.openPopup("schedulePopup","Room 1: Questions & Answers\n\nRoom 2: Main Meetup\n\nRoom 3: Lightning\n\nBeach: Random Talk",[]);
+    currentPopup =  WA.ui.openPopup("schedulePopup","Room 1: Q & A\n\nRoom 2: Main Meetup\n\nRoom 3: Lightning\n\nBeach: Random Talk",[]);
 })
-
 WA.room.onLeaveLayer('scheduleZone').subscribe(closePopUp)
 
 WA.room.onEnterLayer('boatingZone').subscribe(() => {
-    console.log('toto')
     currentPopup =  WA.ui.openPopup("boatingPopup","OH NO!!!\n\nSomeone had a boating accident?",[]);
 })
-
 WA.room.onLeaveLayer('boatingZone').subscribe(closePopUp)
+
+WA.room.onEnterLayer('citadelZone').subscribe(() => {
+    currentPopup =  WA.ui.openPopup("citadelPopup","Nice draft for a citadel!",[]);
+})
+WA.room.onLeaveLayer('citadelZone').subscribe(closePopUp)
+
+WA.room.onEnterLayer('portZone').subscribe(() => {
+    currentPopup =  WA.ui.openPopup("portPopup","Finally a safehaven.",[]);
+})
+WA.room.onLeaveLayer('portZone').subscribe(closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
